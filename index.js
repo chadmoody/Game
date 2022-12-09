e8d3b9dfad56d99fd98c37e1886b2c76f57b2f99
chadmoody <80466009+chadmoody@users.noreply.github.com>
2022-12-09 08:54:34 -0500

    Add files via upload
    fuck

--- index.js
+++ index.js
@@ -1,158 +1,392 @@
-var global_context = null;
-            var tileW = 40;
-            var tileH = 40;
-            var mapW = 10;
-            var mapH = 10;
-            var borderX = tileW*(mapW-1)+8
-            var borderY = tileH*(mapH-1)+8
-            var tilemap =[];
-            
-           
-           
-            var gameMap = [
-            0,0,0,0,0,0,0,0,0,0,
-            0,0,0,1,1,0,0,1,0,0,
-            0,0,1,1,0,0,0,1,1,0,
-            0,1,1,1,1,1,1,1,0,0,
-            0,0,0,0,1,0,1,1,0,0,
-            0,0,1,1,1,1,1,0,0,0,
-            0,1,1,0,0,0,1,0,0,0,
-            0,0,1,0,0,0,1,0,0,0,
-            0,1,1,0,0,0,1,1,1,0,
-            0,0,0,0,0,0,0,0,0,0,
-            ];
-            window.onload = function(){
-                global_context = document.getElementById('game').getContext('2d');
-                
-                requestAnimationFrame(Game);
-                global_context - 'bold 10pt sans-serif';
-               
-            };
-         
+const questions = [
+    {
+        question: "How many days makes a week ?",
+        optionA: "10 days",
+        optionB: "14 days",
+        optionC: "5 days",
+        optionD: "7 days",
+        correctOption: "optionD"
+    },
 
-            function Game(){
-                if(global_context==null){
-                    return;
-                }
-               for(var i = 0; i < mapH; i++){
-                    for(var x = 0; x < mapW; x++){
-                        switch(gameMap[((i*mapW)+ x)])
-                        {
-                            case 0:
-                                global_context.fillStyle = '#000000';
-                                var n = 0;
-                                break;
-                            default:
-                                global_context.fillStyle = "#59ff00";
-                                var n = 1;
-                        }
-                        global_context.fillRect(x*tileW, i*tileH, tileW, tileH);
-                        var tl = new tile(x*tileW, i*tileH,n);
-                        tilemap.push(tl);
-                    }
-                }
-              
-                
-                
+    {
+        question: "How many players are allowed on a soccer pitch ?",
+        optionA: "10 players",
+        optionB: "11 players",
+        optionC: "9 players",
+        optionD: "12 players",
+        correctOption: "optionB"
+    },
 
-                global_context.fillStyle = "#ff0000";
-               
+    {
+        question: "Who was the first President of USA ?",
+        optionA: "Donald Trump",
+        optionB: "Barack Obama",
+        optionC: "Abraham Lincoln",
+        optionD: "George Washington",
+        correctOption: "optionD"
+    },
 
-                requestAnimationFrame(Game);
+    {
+        question: "30 days has ______ ?",
+        optionA: "January",
+        optionB: "December",
+        optionC: "June",
+        optionD: "August",
+        correctOption: "optionC"
+    },
 
-            }
+    {
+        question: "How manay hours can be found in a day ?",
+        optionA: "30 hours",
+        optionB: "38 hours",
+        optionC: "48 hours",
+        optionD: "24 hours",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which is the longest river in the world ?",
+        optionA: "River Nile",
+        optionB: "Long River",
+        optionC: "River Niger",
+        optionD: "Lake Chad",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "_____ is the hottest Continent on Earth ?",
+        optionA: "Oceania",
+        optionB: "Antarctica",
+        optionC: "Africa",
+        optionD: "North America",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which country is the largest in the world ?",
+        optionA: "Russia",
+        optionB: "Canada",
+        optionC: "Africa",
+        optionD: "Egypt",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of these numbers is an odd number ?",
+        optionA: "Ten",
+        optionB: "Twelve",
+        optionC: "Eight",
+        optionD: "Eleven",
+        correctOption: "optionD"
+    },
+
+    {
+        question: `"You Can't see me" is a popular saying by`,
+        optionA: "Eminem",
+        optionB: "Bill Gates",
+        optionC: "Chris Brown",
+        optionD: "John Cena",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Where is the world tallest building located ?",
+        optionA: "Africa",
+        optionB: "California",
+        optionC: "Dubai",
+        optionD: "Italy",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The longest river in the United Kingdom is ?",
+        optionA: "River Severn",
+        optionB: "River Mersey",
+        optionC: "River Trent",
+        optionD: "River Tweed",
+        correctOption: "optionA"
+    },
 
 
-            function isCollide(a, b) {
-              return !(
-                  ((a.y + a.height) < (b.y)) ||
-                  (a.y > (b.y + b.height)) ||
-                  ((a.x + a.width) < b.x) ||
-                  (a.x > (b.x + b.width))
-              );
-          }
+    {
+        question: "How many permanent teeth does a dog have ?",
+        optionA: "38",
+        optionB: "42",
+        optionC: "40",
+        optionD: "36",
+        correctOption: "optionB"
+    },
 
-          function playerupdate(x, y) {
-            $("#player").css('left', x + 'px');
-            $("#player").css('top', y + 'px');
+    {
+        question: "Which national team won the football World cup in 2018 ?",
+        optionA: "England",
+        optionB: "Brazil",
+        optionC: "Germany",
+        optionD: "France",
+        correctOption: "optionD"
+    },
 
-            $("#txt").html('x: ' + x + ' y: ' + y);
+    {
+        question: "Which US state was Donald Trump Born ?",
+        optionA: "New York",
+        optionB: "California",
+        optionC: "New Jersey",
+        optionD: "Los Angeles",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How man states does Nigeria have ?",
+        optionA: "24",
+        optionB: "30",
+        optionC: "36",
+        optionD: "37",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "____ is the capital of Nigeria ?",
+        optionA: "Abuja",
+        optionB: "Lagos",
+        optionC: "Calabar",
+        optionD: "Kano",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Los Angeles is also known as ?",
+        optionA: "Angels City",
+        optionB: "Shining city",
+        optionC: "City of Angels",
+        optionD: "Lost Angels",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What is the capital of Germany ?",
+        optionA: "Georgia",
+        optionB: "Missouri",
+        optionC: "Oklahoma",
+        optionD: "Berlin",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "How many sides does an hexagon have ?",
+        optionA: "Six",
+        optionB: "Sevene",
+        optionC: "Four",
+        optionD: "Five",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How many planets are currently in the solar system ?",
+        optionA: "Eleven",
+        optionB: "Seven",
+        optionC: "Nine",
+        optionD: "Eight",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which Planet is the hottest ?",
+        optionA: "Jupitar",
+        optionB: "Mercury",
+        optionC: "Earth",
+        optionD: "Venus",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "where is the smallest bone in human body located?",
+        optionA: "Toes",
+        optionB: "Ears",
+        optionC: "Fingers",
+        optionD: "Nose",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "How many hearts does an Octopus have ?",
+        optionA: "One",
+        optionB: "Two",
+        optionC: "Three",
+        optionD: "Four",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How many teeth does an adult human have ?",
+        optionA: "28",
+        optionB: "30",
+        optionC: "32",
+        optionD: "36",
+        correctOption: "optionC"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 20) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
         }
-         function checktile(t){
-            if((t.x == player_obj.x)&&(t.y == player_obj.y)){
-                if(t.type == 1){
-                    alert('test');
-                    playerupdate(player_obj.past_x,player_obj.past_y);
-                }
-            }
-         }
-      
-            class playerobj{
-              constructor(obj){
-                this.x = parseInt($(obj).css('left'));
-                this.y = parseInt($(obj).css('top'));
-                this.width = $(obj).width();
-                this.height = $(obj).height();
-                this.past_x = this.x;
-                this.past_y = this.y;
-                this.battery = 100;
-                this.health = 100;
-              }
-            }
+    }
+}
 
 
-            class tile{
-                constructor(x, y, num){
-                  this.x = x;
-                  this.y = y;
-                  this.type = num;
-                }
-              }
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
 
 
-            var player_obj = new playerobj("#player");
-            $(document).ready(function(){
-                
-                player_obj.x = 8;
-                player_obj.y = 8;
-                playerupdate(player_obj.x,player_obj.y);
-                
-                
-            });
-            
-          
-              $(document).keydown(function(e){
-                  
-                  var key_code = e.which || e.keyCode;
-                  switch (key_code) {
-                    case 37: //left arrow key
-                    if (player_obj.x > 8){
-                    player_obj.past_x = player_obj.x;
-                    player_obj.x -= 40;
-                    playerupdate(player_obj.x,player_obj.y);
-                }
-                    break;
-                case 38: //Up arrow key
-                    if (player_obj.y > 8){
-                    player_obj.past_y = player_obj.y;
-                    player_obj.y -= 40;
-                    playerupdate(player_obj.x,player_obj.y);
-                }
-                    break;
-                case 39: //right arrow key
-                    if (player_obj.x < borderX){
-                    player_obj.past_x = player_obj.x;
-                    player_obj.x += 40;
-                    playerupdate(player_obj.x,player_obj.y);
-                }
-                    break;
-                case 40: //down arrow key
-                    if (player_obj.y < borderY){
-                    player_obj.past_y = player_obj.y;
-                    player_obj.y += 40;
-                    playerupdate(player_obj.x,player_obj.y);
-                }
-                    break;
-              }
-              tilemap.forEach(checktile)
-          });
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+        
+    })
+    $("main").toggle();
+    $("#wrapper").toggle();
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (playerScore <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
